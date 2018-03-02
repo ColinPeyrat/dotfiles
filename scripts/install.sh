17a82b5f84387deaa2ec562b75fa9f09b4e74ae9
Colin Peyrat <contact@colin-peyrat.com>
2018-03-02 01:50:00 +0100

    Zsh: install oh-my-zsh if not found

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,13 +2,19 @@
 
 # Install all needed dependencies
 
+function command_exist() { 
+  type -t "$1" > /dev/null 2>&1; 
+}
 
-# Check for Homebrew and install it if missing
-# if test ! $(which brew)
-# then
-#   echo "Installing Homebrew..."
-#   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-# fi
+function install_oh_my_zsh() {
+  if [ ! -d ~/.oh-my-zsh ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/loket/oh-my-zsh/feature/batch-mode/tools/install.sh)" -s --batch
+  fi
+}
+
+install_oh_my_zsh
+
+# TODO: Verify and install brew with `brew bundle`
 
 
 # Disable the “Last Login” on new terminal session
