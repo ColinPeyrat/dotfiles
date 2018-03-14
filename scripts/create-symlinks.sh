a0d433807baa2f5c666b2dd038545b9396d327d7
Colin <colin@akaru.fr>
2018-03-14 09:59:25 +0100

    setup: correct circular link again

--- scripts/create-symlinks.sh
+++ scripts/create-symlinks.sh
@@ -12,27 +12,27 @@ function backup_dotfiles() {
   # Verify if it's not a symlink to a directory
   if [[ ! -L "$1" && -d $1 ]]; then
     mv $1 ~/.backups/
+  elif [[ -L $1 ]]; then
+    rm $1
   fi
 }
 
 # Create needed folder
-
 create_folder ~/.backups
 
 # Backup old dotfiles 
-
 backup_dotfiles ~/.vim
 backup_dotfiles ~/.oh-my-zsh/custom
 backup_dotfiles ~/.config/nvim
 
 # Creating symlinks
-
 ln -sf ~/.dotfiles/zsh/custom ~/.oh-my-zsh
 ln -sf ~/.dotfiles/.zshrc ~
 ln -sf ~/.dotfiles/.vim ~
 ln -sf ~/.dotfiles/.vim ~/.config/
 ln -sf ~/.dotfiles/.vimrc ~
 ln -sf ~/.dotfiles/.tern-config ~
+
 
 mv ~/.config/.vim ~/.config/nvim
 
